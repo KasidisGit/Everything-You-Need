35d7bbaeeb4e37687da3e522e4d9a3731304e28a
KasidisGit <kasidis02@gmail.com>
2021-05-17 01:12:11 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,82 +1,11 @@
-import { useState } from 'react';
-import './App.css';
-const axios = require('axios');
-
-const initialState = {
-    username: "",
-    cash: 0, 
-    email: "", 
-    role: "", 
-    password: "" 
-};
+import RegisterComponent from './components/registerComponent.js';
 
 function App() {
-
-  const [formState, setFormState] = useState(initialState);
-
-  const submitHandler = event => {
-    event.preventDefault();
-    postData();
-  };
-
-  const postData = () => {
-    const json = JSON.stringify(formState);
-    axios.post(
-      'http://localhost:9000/api/v1/users/register', json, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin' : '*'
-        }
-      });
-  };
 
   return (
     <div className="App">
       <header className="App-header">
-
-      <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        placeholder="Name"
-        value={formState.username}
-        onChange={e => {
-          setFormState({ ...formState, username: e.target.value });
-        }}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={formState.email}
-        onChange={e => {
-          setFormState({ ...formState, email: e.target.value });
-        }}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={formState.password}
-        onChange={e => {
-          setFormState({ ...formState, password: e.target.value });
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Role"
-        value={formState.role}
-        onChange={e => {
-          setFormState({ ...formState, role: e.target.value });
-        }}
-      />
-      <input
-        type="number"
-        placeholder="Cash"
-        value={formState.cash}
-        onChange={e => {
-          setFormState({ ...formState, cash: e.target.value });
-        }}
-      />
-      <button type="submit" >Confirm</button>
-    </form>
+      <RegisterComponent />
       </header>
     </div>
   );
